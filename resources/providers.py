1dee68980dd925a4de2fae2138317ff7289015b3
Malcom Lipscomb Jr <malcomlipscombjr.@Malcoms-MacBook.local>
2020-02-13 16:37:02 -0700

    updated providers routes...not tested

--- resources/providers.py
+++ resources/providers.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from playhouse.shortcuts import model_to_dict
 from flask_bcrypt import generate_password_hash, check_password_hash
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 import models
 
@@ -45,3 +45,54 @@ def logout():
     name = model_to_dict(current_user)['name']
     logout_user()
     return jsonify(data = {}, status = {'code': 200, 'message': f'{name} has logged out'}) 
+
+
+#index route to show all provider's active patients
+@providers.route('/', methods = ["GET"])
+@login_required
+def get_all_patients():
+    try:
+        patients = [model_to_dict(patient) for patient in models.Patient.select().where(models.Patient.symptoms_id == current_user.id)]
+        for patient in patients:
+            patient['diagnoses'].pop('password')
+        return jsonify(data = patients, status = {'code': 200, 'message': 'Success finding the patients'})
+    except models.DoesNotExist:
+        return jsonify(data = {}, status = {'code': 400, 'message': 'Error getting the patients'})
+
+
+
+#show route...access a specific providers's account upon login
+@providers.route('/<id>', methods = ["GET"])
+@login_required
+def get_one_provider(id):
+    try:
+        providers = models.Providers.get_by_id(id)
+        providers_dict = model_to_dict(providers)
+        return jsonify(data = providers_dict, status = {'code': 200, 'message': f'Found providers with id {providers.id}'})
+    except models.DoesNotExist:
+        return jsonify(data = {}, status = {'code': 400, 'message': 'error finding the providers'})
+
+
+#create route for symptoms/health issues
+@providers.route('/', methods = ["POST"])
+@login_required
+def create_diagnosis():
+    try:
+        payload = request.get_json()
+        payload['diagnoses'] = current_user.id
+        providers = models.Providers.create(**payload)
+        providers_dict = model_to_dict(providers)
+        return jsonify(data = providers_dict, status = {'code': 200, 'message': "Diagnosis added"})
+    except models.DoesNotExist:
+        return jsonify(data = {}, status = {'code': 400, 'message': 'error updatingproviders symptoms'})
+
+
+#delete route for deleting health issues
+@providers.route('/<id>', methods = ["DELETE"])
+def delete_diagnosis(id):
+    try:
+        query = models.Providers.delete()['diagnoses'].where(models.Providers.id == id)
+        query.execute()
+        return jsonify(data = 'issues successfully deleted', status = {'code': 200, 'message': 'Issues successfully deleted'})
+    except models.DoesNotExist:
+        return jsonify(data = {}, status = {'code': 400, 'message': 'error deleting the issues'})
